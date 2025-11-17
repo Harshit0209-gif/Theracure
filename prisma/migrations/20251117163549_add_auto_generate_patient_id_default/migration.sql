8e0104c1388834f9f2ae05c11193952f631a877e
Bhaskar Gayen <gayenbhaskar007@gmail.com>
2025-11-17 23:46:14 +0530

    fix: new patient creation with sequence ID

--- prisma/migrations/20251117163549_add_auto_generate_patient_id_default/migration.sql
+++ prisma/migrations/20251117163549_add_auto_generate_patient_id_default/migration.sql
@@ -1,2 +1,2 @@
--- AlterTable
-ALTER TABLE "patients" ALTER COLUMN "patient_id" SET DEFAULT auto_generate_patient_id();
+-- AlterTable - Use generate_patient_id() instead of auto_generate_patient_id() (which is a trigger function)
+ALTER TABLE "patients" ALTER COLUMN "patient_id" SET DEFAULT generate_patient_id();
